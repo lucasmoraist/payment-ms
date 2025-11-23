a2393c8dbca7bed04057770b1539a064ed159a71
lucasmoraist <luksmnt1101@gmail.com>
2025-11-23 14:05:49 -0300

    feat: refactor PaymentController to use Lombok and add logging

--- src/main/java/com/lucasmoraist/payment_ms/infrastructure/web/controller/PaymentController.java
+++ src/main/java/com/lucasmoraist/payment_ms/infrastructure/web/controller/PaymentController.java
@@ -6,19 +6,18 @@ import com.lucasmoraist.payment_ms.domain.model.Payment;
 import com.lucasmoraist.payment_ms.infrastructure.service.CertificateService;
 import com.lucasmoraist.payment_ms.infrastructure.web.dto.PaymentRequest;
 import com.lucasmoraist.payment_ms.infrastructure.web.routes.PaymentRoutes;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
+@Log4j2
 @RestController
+@RequiredArgsConstructor
 public class PaymentController implements PaymentRoutes {
 
     private final ProcessPaymentCase processPaymentCase;
     private final CertificateService certificateService;
-
-    public PaymentController(ProcessPaymentCase processPaymentCase, CertificateService certificateService) {
-        this.processPaymentCase = processPaymentCase;
-        this.certificateService = certificateService;
-    }
 
     @Override
     public ResponseEntity<Void> createPayment(PaymentRequest request, String payloadHash) {
@@ -34,6 +33,7 @@ public class PaymentController implements PaymentRoutes {
         );
 
         this.processPaymentCase.execute(payment);
+        log.info("Payment processed successfully for payment id");
         return ResponseEntity.status(201).build();
     }
 
